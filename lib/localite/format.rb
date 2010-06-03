ee2cf3df54222c07a74650111674cd03522c77dc
eno <eno@open-lab.org>
2010-06-03 17:22:54 +0200

    Fixes html/text encoding

--- lib/localite/format.rb
+++ lib/localite/format.rb
@@ -3,16 +3,16 @@ module Localite::Format
   # convert from text into target format
   def self.text(s)
     case s
-    when /html:(.*)/  then $1
-    when /text:(.*)/  then $1
+    when /html:(.*)/m then $1
+    when /text:(.*)/m then $1
     else              s
     end
   end
 
   def self.html(s)
     case s
-    when /html:(.*)/  then $1
-    when /text:(.*)/  then CGI.escapeHTML($1)
+    when /html:(.*)/m then $1
+    when /text:(.*)/m then CGI.escapeHTML($1)
     else              CGI.escapeHTML(s)
     end
   end
@@ -27,6 +27,10 @@ module Localite::Format::Etest
     assert_equal "&lt;&gt;", Localite::Format.html("text:<>")
     assert_equal "&lt;&gt;", Localite::Format.html("<>")
     assert_equal "<>", Localite::Format.html("html:<>")
+
+    assert_equal "xxx\nyyy", Localite::Format.html("xxx\nyyy")
+    assert_equal "xxx\nyyy", Localite::Format.html("html:xxx\nyyy")
+    assert_equal "xxx\nyyy", Localite::Format.html("text:xxx\nyyy")
   end
 end
 
